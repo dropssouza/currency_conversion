7e09c56741bd079be9110c28ba525d2089796858
Pedro Souza <pedrohenrique_2010@hotmail.com>
2025-09-03 14:45:01 -0300

    atualizacao_front

--- frontend/script.js
+++ frontend/script.js
@@ -8,6 +8,9 @@ botao.addEventListener("touchend", () => {
   botao.style.transform = "scale(1)";
 });
 
+const API_URL = "https://currency-conversion-srm9.onrender.com";
+//const API_URL = "http://127.0.0.1:8000";
+
 async function calcular() {
   const moeda1 = document.getElementById("moeda1").value;
   const moeda2 = document.getElementById("moeda2").value;
@@ -18,9 +21,19 @@ async function calcular() {
     return;
   }
 
+  const botao = document.getElementById("botao_converter");
+
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
+
+  botao.disabled = true;
+  botao.style.opacity = "0.6";
+  botao.style.visibility = "hidden";
+  botao.parentElement.appendChild(spinner);
+
   try {
     const response = await fetch(
-      `http://127.0.0.1:8000/converter?moeda_origem=${moeda1}&moeda_destino=${moeda2}&valor=${valor1}`
+      `${API_URL}/converter?moeda_origem=${moeda1}&moeda_destino=${moeda2}&valor=${valor1}`
     );
     const data = await response.json();
 
@@ -29,4 +42,9 @@ async function calcular() {
     console.error("Erro na conversão:", error);
     alert("Erro ao conectar com o servidor");
   }
+
+  spinner.remove();
+  botao.disabled = false;
+  botao.style.opacity = "1";
+  botao.style.visibility = "visible";
 }
